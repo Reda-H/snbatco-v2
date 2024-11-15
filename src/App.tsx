658a85d0b1e47f072b8542ed92e37df10c74a51a
Reda-H <Herradi.R@gmail.com>
2024-11-15 17:31:41 +0100

    added 404 bucket route

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import Contact from './components/Contact';
 const router = createBrowserRouter([
   { path: '/', element: <Home /> },
   { path: '/about', element: <About /> },
+  { path: '*', element: <div>404</div> }
 ]);
 
 function App() {
